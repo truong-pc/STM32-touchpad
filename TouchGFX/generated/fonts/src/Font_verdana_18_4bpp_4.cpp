45471e6a4b9dfcba506115b2de3ec31ce707ff10
Truong Phan <137706113+truong-pc@users.noreply.github.com>
2026-07-02 19:04:49 +0700

    initial commit

---
+++ TouchGFX/generated/fonts/src/Font_verdana_18_4bpp_4.cpp
@@ -0,0 +1,7 @@
+#include <touchgfx/hal/Types.hpp>
+
+FONT_GLYPH_LOCATION_FLASH_PRAGMA
+KEEP extern const uint8_t unicodes_verdana_18_4bpp_4[] FONT_GLYPH_LOCATION_FLASH_ATTRIBUTE = {
+    // Unicode: [0x2212]
+    0x70, 0x77, 0x77, 0x77, 0x77, 0x57, 0xF1, 0xFF, 0xFF, 0xFF, 0xFF, 0xCF
+};
